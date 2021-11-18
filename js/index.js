4b67747c20d53fa4b4f8e5c9ae4bdb6fd12d207c
rcejudo <rocio.cejudo.soronellas@gmail.com>
2021-11-18 03:46:12 +0100

    end accordion slider

--- js/index.js
+++ js/index.js
@@ -6,27 +6,29 @@ $(document).ready(function () {
 });
 
 // slider prueba 3
-$(function () { 
-  $("section .info").first().show().animate({
-    width: "79%",
-  });
-  $("section .slide").click(function () {
-    $(this)
-      .next()
-      .show()
-      .animate({
-        width: "79%",
-      })
-      .siblings("section .info")
-      .animate(
-        {
-          width: "0%",
-        },
-        function () {
-          $(this).hide();
-        }
-      );
-    
-    // $(this).addClass("test").siblings().removeClass("test");
+$(document).ready(function() {
+  var $item = 0,
+    $itemNo = $(".banner-slider figure").length;
+  function transitionSlide() {
+    $item++;
+    if ($item > $itemNo - 1) {
+      $item = 0;
+    }
+    $(".banner-slider figure").removeClass("on");
+    $(".banner-slider figure")
+      .eq($item)
+      .addClass("on");
+  }
+  var $autoTransition = setInterval(transitionSlide, 3500);
+
+  $(".banner-slider figure").click(function() {
+    clearInterval($autoTransition);
+    $item = $(this).index();
+    $(".banner-slider figure").removeClass("on");
+    $(".banner-slider figure")
+      .eq($item)
+      .addClass("on");
+    $autoTransition = setInterval(transitionSlide, 3500);
   });
 });
+
